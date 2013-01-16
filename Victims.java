a22099f310fb95887b739aacc57c956e9b5ab128
Deborah Gaanderse <housefreaks@hotmail.com>
2013-01-16 16:08:19 +0100

    Victims class aangemaakt

---
+++ Victims.java
@@ -0,0 +1,15 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * DEZE MENSEN GAAN DOOD EN ALS JE ZE NIET RED DAN IS DAT PECH VOOR ZE.
+ * 
+ * Project 42
+ */
+public class Victims extends Calamities
+{
+
+    public void act() 
+    {
+        // Spawnt
+    }    
+}
