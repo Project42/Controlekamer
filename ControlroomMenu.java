b78012cee819d6a0ebf7c50f5fc6b7a4088b28e1
Deborah Gaanderse <housefreaks@hotmail.com>
2013-01-24 23:22:01 +0100

    Controlroom menu done

--- ControlroomMenu.java
+++ ControlroomMenu.java
@@ -8,14 +8,19 @@ public class ControlroomMenu extends World
 {
     public ControlroomMenu() {
         super(80, 80, 10);
-        setBackground("controlroom_instructions.png");
+        setBackground("menu_controlroom1.png");
     }
 
     @Override
     public void act() {
-        // Starts game when pressed enter
-        if (Greenfoot.isKeyDown("enter")) {
+        // Starts game when pressed enter 2 times
+        if (Greenfoot.isKeyDown("right")) {
+            setBackground("menu_controlroom2.png");
+        } else if (Greenfoot.isKeyDown("enter")) {
             Greenfoot.setWorld(new ControlroomWorld());
+            return;
+        } else if (Greenfoot.isKeyDown("left")) {
+            setBackground("menu_controlroom1.png");
             return;
         }
     }
