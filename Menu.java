9c3905cfc2248881109905501f319dba828106ba
Deborah Gaanderse <housefreaks@hotmail.com>
2013-01-24 18:16:33 +0100

    game = done

--- Menu.java
+++ Menu.java
@@ -17,72 +17,87 @@ public class Menu extends Actor
     public void checkClicked(int character) 
     {
         ControlroomWorld world = (ControlroomWorld)getWorld();
-        if (Greenfoot.mouseClicked(this)) 
-        {
-            
-            if (character == 1) {
-                 world.setSelectedCharacter(ControlroomWorld.Character.FIREFIGHTER);
-                }
-            else if (character == 2) {
-                 world.setSelectedCharacter(ControlroomWorld.Character.POLICE_SHUTOFF);
-            }
-            
-            else if (character == 3) {
-                 world.setSelectedCharacter(ControlroomWorld.Character.POLICE_EVACUATE);
-            }
-            
-            else if (character == 4) {
-                 world.setSelectedCharacter(ControlroomWorld.Character.POLICE_CATCHTHIEF);
-             }
-        }
-        
-        if (Greenfoot.isKeyDown("1")) {
-            world.setSelectedCharacter(ControlroomWorld.Character.FIREFIGHTER);
-        }
-        
-        if (Greenfoot.isKeyDown("2")) {
-            world.setSelectedCharacter(ControlroomWorld.Character.POLICE_SHUTOFF);
-        }
-        
-        if (Greenfoot.isKeyDown("3")) {
-             world.setSelectedCharacter(ControlroomWorld.Character.POLICE_CATCHTHIEF);
-        }
-        
-        if (Greenfoot.isKeyDown("4")) {
-             world.setSelectedCharacter(ControlroomWorld.Character.POLICE_EVACUATE);
-        }
- 
-        if (world.getSelectedCharacter() == ControlroomWorld.Character.FIREFIGHTER) {
-            world.getExtinguishMenu().setImage("firefighter_glow.png");
-            world.getShutOffMenu().setImage(originalShutOff);
-            world.getEvacuateMenu().setImage(originalEvacuate);
-            world.getCatchThiefMenu().setImage(originalCatchThief);
-        }
-        
-        else if (world.getSelectedCharacter() == ControlroomWorld.Character.POLICE_SHUTOFF) {
-            world.getShutOffMenu().setImage("shut_off_street_glow.png");
-            world.getExtinguishMenu().setImage(originalFirefighter);
-            world.getEvacuateMenu().setImage(originalEvacuate);
-            world.getCatchThiefMenu().setImage(originalCatchThief);
-        }
-        
-        else if (world.getSelectedCharacter() == ControlroomWorld.Character.POLICE_EVACUATE) {
-            world.getEvacuateMenu().setImage("evac_vehicle_glow.png");
-            world.getExtinguishMenu().setImage(originalFirefighter);
-            world.getShutOffMenu().setImage(originalShutOff);
-            world.getCatchThiefMenu().setImage(originalCatchThief);
-        }
-        else if (world.getSelectedCharacter() == ControlroomWorld.Character.POLICE_CATCHTHIEF) {
-            world.getCatchThiefMenu().setImage("arrest_glow.png");
-            world.getExtinguishMenu().setImage(originalFirefighter);
-            world.getShutOffMenu().setImage(originalShutOff);
-            world.getEvacuateMenu().setImage(originalEvacuate);
+        if (world.getPoliceUnits().getValue() == 0) {
+                world.getShutOffMenu().setImage("shut_off_street_depleted.png");
+                world.getEvacuateMenu().setImage("evac_vehicle_depleted.png");
+                world.getCatchThiefMenu().setImage("arrest_depleted.png");
         }
         else {
-            world.getExtinguishMenu().setImage(originalFirefighter);
-            world.getShutOffMenu().setImage(originalShutOff);
-            world.getEvacuateMenu().setImage(originalEvacuate);
-            world.getCatchThiefMenu().setImage(originalCatchThief);
+            if (Greenfoot.mouseClicked(this)) 
+            {
+                if (character == 2) {
+                     world.setSelectedCharacter(ControlroomWorld.Character.POLICE_SHUTOFF);
+                }
+                
+                else if (character == 3) {
+                     world.setSelectedCharacter(ControlroomWorld.Character.POLICE_EVACUATE);
+                }
+                
+                else if (character == 4) {
+                     world.setSelectedCharacter(ControlroomWorld.Character.POLICE_CATCHTHIEF);
+                 }
+            }
+      
+            if (Greenfoot.isKeyDown("2")) {
+                world.setSelectedCharacter(ControlroomWorld.Character.POLICE_SHUTOFF);
+            }
+            
+            if (Greenfoot.isKeyDown("3")) {
+                 world.setSelectedCharacter(ControlroomWorld.Character.POLICE_CATCHTHIEF);
+            }
+            
+            if (Greenfoot.isKeyDown("4")) {
+                 world.setSelectedCharacter(ControlroomWorld.Character.POLICE_EVACUATE);
+            }
+                 
+            else if (world.getSelectedCharacter() == ControlroomWorld.Character.POLICE_SHUTOFF) {
+                world.getShutOffMenu().setImage("shut_off_street_glow.png");
+                world.getExtinguishMenu().setImage(originalFirefighter);
+                world.getEvacuateMenu().setImage(originalEvacuate);
+                world.getCatchThiefMenu().setImage(originalCatchThief);
+            }
+            
+            else if (world.getSelectedCharacter() == ControlroomWorld.Character.POLICE_EVACUATE) {
+                world.getEvacuateMenu().setImage("evac_vehicle_glow.png");
+                world.getExtinguishMenu().setImage(originalFirefighter);
+                world.getShutOffMenu().setImage(originalShutOff);
+                world.getCatchThiefMenu().setImage(originalCatchThief);
+            }
+            else if (world.getSelectedCharacter() == ControlroomWorld.Character.POLICE_CATCHTHIEF) {
+                world.getCatchThiefMenu().setImage("arrest_glow.png");
+                world.getExtinguishMenu().setImage(originalFirefighter);
+                world.getShutOffMenu().setImage(originalShutOff);
+                world.getEvacuateMenu().setImage(originalEvacuate);
+            }
+            else {
+                world.getExtinguishMenu().setImage(originalFirefighter);
+                world.getShutOffMenu().setImage(originalShutOff);
+                world.getEvacuateMenu().setImage(originalEvacuate);
+                world.getCatchThiefMenu().setImage(originalCatchThief);
+            }
+        }
+        
+        if (world.getFirefighterUnits().getValue() == 0) {
+             world.getExtinguishMenu().setImage("firefighter_depleted.png");
+        }
+        else {
+          if (Greenfoot.mouseClicked(this)) 
+                {
+                  if (character == 1) {
+                         world.setSelectedCharacter(ControlroomWorld.Character.FIREFIGHTER);
+                  }
+
+
+                }
+          if (Greenfoot.isKeyDown("1")) {
+                world.setSelectedCharacter(ControlroomWorld.Character.FIREFIGHTER);
+          }
+          if (world.getSelectedCharacter() == ControlroomWorld.Character.FIREFIGHTER) {
+                world.getExtinguishMenu().setImage("firefighter_glow.png");
+                world.getShutOffMenu().setImage(originalShutOff);
+                world.getEvacuateMenu().setImage(originalEvacuate);
+                world.getCatchThiefMenu().setImage(originalCatchThief);
+          }
+        }
         }
     }
-}
